7837ffc556a9581f7af232fe231f360d7ba03fa2
NekoSakura <1330040125@qq.com>
2017-10-30 17:52:01 +0800

    后台登陆隐藏

--- wp-content/themes/Beginning/functions/nekomiao.php
+++ wp-content/themes/Beginning/functions/nekomiao.php
@@ -2,12 +2,14 @@
 /**
  * 自定义登陆界面
  */
-function custom_login_logo() {
+function custom_login_logo()
+{
     echo '
-<link rel="stylesheet" id="wp-admin-css" href="'.get_bloginfo('template_directory').'/css/xinadmin.css"
+<link rel="stylesheet" id="wp-admin-css" href="' . get_bloginfo( 'template_directory' ) . '/css/xinadmin.css"
       type="text/css"/>';
 }
-add_action('login_head', 'custom_login_logo');
+
+add_action( 'login_head', 'custom_login_logo' );
 
 /**
  * 自定义 加载Js、Css
@@ -18,7 +20,8 @@ add_action('login_head', 'custom_login_logo');
  * @uses wp_register_script() 注册 JavaScript 脚本
  * @uses wp_enqueue_script()  挂载 JavaScript 脚本
  */
-function Neko_enqueue_scripts() {
+function Neko_enqueue_scripts()
+{
 //    wp_deregister_script('jquery');
 //    wp_register_script('jquery', 'https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js', false);
 //    wp_enqueue_script('jquery');
@@ -27,6 +30,7 @@ function Neko_enqueue_scripts() {
     wp_enqueue_script( 'nekomiao', get_template_directory_uri() . '/js/nekomiao.js', ['jquery'], THEME_DB_VERSION );
 
 }
+
 add_action( 'wp_enqueue_scripts', 'Neko_enqueue_scripts' );
 
 
@@ -34,43 +38,56 @@ add_action( 'wp_enqueue_scripts', 'Neko_enqueue_scripts' );
  * 去除控制台显示的jQuery迁徙
  *
  */
-add_action( 'wp_default_scripts', function( $scripts ) {
-    if ( ! empty( $scripts->registered['jquery'] ) ) {
+add_action( 'wp_default_scripts', function ( $scripts ) {
+    if ( !empty( $scripts->registered['jquery'] ) ) {
         $jquery_dependencies = $scripts->registered['jquery']->deps;
-        $scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
+        $scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array('jquery-migrate') );
     }
 } );
 
 //压缩html代码
-function wp_compress_html(){
-    function wp_compress_html_main ($buffer){
-        $initial=strlen($buffer);
-        $buffer=explode("<!--wp-compress-html-->", $buffer);
-        $count=count ($buffer);
-        $buffer_out ='';
+function wp_compress_html()
+{
+    function wp_compress_html_main( $buffer )
+    {
+        $initial = strlen( $buffer );
+        $buffer = explode( "<!--wp-compress-html-->", $buffer );
+        $count = count( $buffer );
+        $buffer_out = '';
 
-        for ($i = 0; $i <= $count; $i++){
-            if (stristr($buffer[$i], '<!--wp-compress-html no compression-->')) {
-                $buffer[$i]=(str_replace("<!--wp-compress-html no compression-->", " ", $buffer[$i]));
+        for ( $i = 0; $i <= $count; $i++ ) {
+            if ( stristr( $buffer[$i], '<!--wp-compress-html no compression-->' ) ) {
+                $buffer[$i] = (str_replace( "<!--wp-compress-html no compression-->", " ", $buffer[$i] ));
             } else {
-                $buffer[$i]=(str_replace("\t", " ", $buffer[$i]));
-                $buffer[$i]=(str_replace("\n\n", "\n", $buffer[$i]));
-                $buffer[$i]=(str_replace("\n", "", $buffer[$i]));
-                $buffer[$i]=(str_replace("\r", "", $buffer[$i]));
-                while (stristr($buffer[$i], '  ')) {
-                    $buffer[$i]=(str_replace("  ", " ", $buffer[$i]));
+                $buffer[$i] = (str_replace( "\t", " ", $buffer[$i] ));
+                $buffer[$i] = (str_replace( "\n\n", "\n", $buffer[$i] ));
+                $buffer[$i] = (str_replace( "\n", "", $buffer[$i] ));
+                $buffer[$i] = (str_replace( "\r", "", $buffer[$i] ));
+                while ( stristr( $buffer[$i], '  ' ) ) {
+                    $buffer[$i] = (str_replace( "  ", " ", $buffer[$i] ));
                 }
             }
-            $buffer_out.=$buffer[$i];
+            $buffer_out .= $buffer[$i];
         }
-        $final=strlen($buffer_out);
-        $savings=($initial-$final)/$initial*100;
-        $savings=round($savings, 2);
-        $buffer_out.="\n<!--压缩前的大小: $initial bytes; 压缩后的大小: $final bytes; 节约：$savings% -->";
+        $final = strlen( $buffer_out );
+        $savings = ($initial - $final) / $initial * 100;
+        $savings = round( $savings, 2 );
+        $buffer_out .= "\n<!--压缩前的大小: $initial bytes; 压缩后的大小: $final bytes; 节约：$savings% -->";
         return $buffer_out;
     }
-    ob_start("wp_compress_html_main");
+
+    ob_start( "wp_compress_html_main" );
 }
-add_action('get_header', 'wp_compress_html');
+
+add_action( 'get_header', 'wp_compress_html' );
+
+
+function login_protection()
+{
+    if ( $_GET["入口"] != "芝麻开门" ) header( "Location: https://www.nekomiao.cn" );
+}
+
+add_action( "login_enqueue_scripts", "login_protection" );
+
 
 //End of page.
